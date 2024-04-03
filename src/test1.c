9424a70b576af659a9de6493aa620781fbc7ed8d
luot <1035768203@qq.com>
2024-04-03 18:17:50 +0800

    add inline

--- src/test1.c
+++ src/test1.c
@@ -3,6 +3,19 @@
 #include "gem5/m5ops.h"
 int main()
 {
-    float test_array0[8] = {1.0, 2.0, 3.0, 4.0};
-    test_inline(test_array0);
+    float* ifmap = calloc(16, sizeof(float));
+    float* kernel = calloc(16, sizeof(float));
+    float* ofmap1 = calloc(4, sizeof(float));
+    float* ofmap2 = calloc(4, sizeof(float));
+    for (int i = 0; i < 16; i++)
+    {
+        ifmap[i] = rand() % 10;
+        kernel[i]= rand() % 10; 
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        ofmap1[i] = 1.0;
+        ofmap2[i] = 1.0;
+    }
+    test_inline2(ifmap,kernel,ofmap1,ofmap2);
 }
