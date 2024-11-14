c7435a078615a9b26e67e56d6a1c6a701ffe7f56
luot <1035768203@qq.com>
2024-11-14 11:52:21 +0800

    little change

--- src/winomain.c
+++ src/winomain.c
@@ -25,6 +25,7 @@ int main(int argc, char *argv[]) {
     // 产生数据
     int m = 2, r = 3;
 
+    //测试环境下：由脚本给出参数
     //int w = atoi(argv[1]), h = atoi(argv[1]), c = atoi(argv[2]), n = atoi(argv[3]), groups = 1, size = 3, stride = 1, pad = 0;
     int w = 48, h = 48, c = 1, n = 1, groups = 1, size = 3, stride = 1, pad = 0;
 
