6af40f5bdf4d8f7a564d9e767c3fcbd833535124
luot <1035768203@qq.com>
2024-11-16 00:55:51 +0800

    test mod

---
+++ src/testmod.h
@@ -0,0 +1,7 @@
+#ifndef __TESTMOD_H__
+#define __TESTMOD_H__
+static void set_mod_reg(int val);
+static int add_mod(int val1,int val2);
+static int sub_mod(int val1,int val2);
+static int mul_mod(int val1,int val2);
+#endif /* __TESTMOD_H__ */      
